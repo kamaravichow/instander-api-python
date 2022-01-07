45afc9523aa5d9482b1d089d944c77c6c8f3b01d
Aravind Chowdary <kamaravichow@gmail.com>
2022-01-08 00:00:36 +0530

    complete new endpoints

--- app.py
+++ app.py
@@ -12,6 +12,34 @@ from instaloader import Profile
 app = Flask(__name__)
 loader = instaloader.Instaloader()
 
+
+def saveSessionTempFile(filename, session):
+    dirname = os.path.dirname(filename)
+    if dirname != '' and not os.path.exists(dirname):
+            os.makedirs(dirname)
+    with open(filename, 'wb') as file:
+        pickle.dump(session, file)
+
+
+def saveSession(json, filename):
+    session = {
+        'sessionid': json['sessionid'],
+        'mid': json['mid'],
+        'ig_pr': '1',
+        'ig_nrcb' : json['ig_nrcb'],
+        'ig_vw': '1920', 
+        'csrftoken': json['csrftoken'],
+        's_network': '', 
+        'ds_user_id': json['ds_user_id'],
+        'ig_did' : json['ig_did'],
+        'shbid' : json['shbid'],
+        'rur' : json['rur'],
+        'shbts' : json['shbts'],
+    }
+    saveSessionTempFile(filename, session)
+
+        
+
 @app.route('/', methods=['GET'])
 def home():
     return render_template('home.html')
@@ -19,7 +47,7 @@ def home():
 
 
 # API Version 1 Methods ----------------------
-@app.route('/api/v1/finder/', methods=['GET'])
+@app.route('/api/v1/locate/', methods=['GET'])
 def application():
     ipadd = request.args['ip']
     try:
@@ -29,183 +57,206 @@ def application():
         return {'status': 'Could not connect to the server'}, 404
 
 
-@app.route('/api/v1/session/', methods=['GET', 'POST'])
-def session() :
-    username = request.args['q']
-    crftoken = request.args['token']
-    ds_user_id = request.args['id']
-    ig_cb = request.args['ig']
-    mid = request.args['mid']
-    sessionid = request.args['session']
-    ig_did = request.args['did']
-    shbid = request.args['bid']
-    shbts = request.args['bts']
-    rur = request.args['rur']
-
-    # Session object
-    session = {
-        'sessionid': sessionid, 
-        'mid': mid, 
-        'ig_pr': '1',
-        'ig_nrcb' : ig_cb,
-        'ig_vw': '1920', 
-        'csrftoken': crftoken,
-        's_network': '', 
-        'ds_user_id': ds_user_id,
-        'ig_did' : ig_did,
-        'shbid' : shbid,
-        'rur' : rur,
-        'shbts' : shbts
-    }
-
+@app.route('/api/v1/profile/basic/', methods=['GET'])
+def profile_basic():
+    username = request.args['username']
+    cookie = json.loads(request.data)
     filename = './sessions/' + username
-    dirname = os.path.dirname(filename)
-
-    if dirname != '' and not os.path.exists(dirname):
-            os.makedirs(dirname)
-            
-    with open(filename, 'wb') as sessionfile:
-        pickle.dump(session, sessionfile)
-        print("saved session file")
-
-    loader.load_session_from_file(username=username, filename='./sessions/' + username)
-    return {"logged_in": loader.test_login()}, 200
-
-
-@app.route('/api/v1/session/check/', methods=['GET', 'POST'])
-def checkSession() :
-    username = request.args['q']
+    saveSession(cookie, filename)
     try:
         loader.load_session_from_file(username=username, filename='./sessions/' + username)
-        return {'username' : str(username), 'status': str(loader.test_login())}, 200
-    except Exception as e :
-        return {'username' : str(username), 'status': 'Session Expired'}, 404
-
-
-
-@app.route('/api/v1/profile/', methods=['GET'])
-def getProfile():
-    username = request.args['q']
-    try :
-        loader.load_session_from_file(username=username, filename='./sessions/' + username)
-    except FileNotFoundError:
-        return {'message' : "Relogin", 'status': 'Session Expired'}, 404
-    try :
         profile = Profile.from_username(loader.context, username)
-
-        prof = {
-            "key" : profile.username,
-            "name" : profile.full_name,
-            "username" : profile.username,
-            "followers" : profile.followers,
-            "followees" : profile.followees,
-            "profile_pic": profile.profile_pic_url,
-            "private" : str(profile.is_private),
-            "verified": str(profile.is_verified),
-        }
-
-        return jsonify(prof), 200
-    except Exception as e :
-        return jsonify({"message" : str(e)}, 404)
-
+        return {
+            'status': 'OK', 'name': str(profile.full_name), 
+            'profile_pic': str(profile.profile_pic_url), 
+            'biography': str(profile.biography),
+            'followers': str(profile.followers),
+            'following': str(profile.followees),
+            'posts': str(profile.mediacount),
+            'private': str(profile.is_private),
+            'verified': str(profile.is_verified),
+            'username': str(profile.username),
+            'website': str(profile.external_url),
+        }, 200
+    except Exception as e:
+        return {'status': 'Invalid username'}, 404
 
 @app.route('/api/v1/profile/posts/', methods=['GET'])
-def getPosts():
-    username = request.args['q']
-    loader.load_session_from_file(username=username, filename='./sessions/' + username)
-    max = 5
-    counter = 0
-    profile = Profile.from_username(loader.context, username)
-    post_iterator = profile.get_posts()
+def profile_posts():
+    username = request.args['username']
+    cookie = json.loads(request.data)
+    filename = './sessions/' + username
+    saveSession(cookie, filename)
+    try:
+        loader.load_session_from_file(username=username, filename='./sessions/' + username)
+        profile = Profile.from_username(loader.context, username)
+        posts = profile.get_posts()
+        return {
+            'status': 'OK', 
+            'posts': [{
+                'id': str(post.shortcode),
+                'likes': str(post.likes),
+                'comments': str(post.comments),
+                'date': str(post.date),
+                'caption': str(post.caption),
+                'url': str(post.url),
+            } for post in posts]
+        }, 200
+    except Exception as e:
+        return {'status': 'Invalid request'}, 404
 
-    post_serialisable_list = []
+@app.route('/api/v1/profile/followers/', methods=['GET'])
+def profile_followers():
+    username = request.args['username']
+    cookie = json.loads(request.data)
+    filename = './sessions/' + username
+    saveSession(cookie, filename)
+    try:
+        loader.load_session_from_file(username=username, filename='./sessions/' + username)
+        profile = Profile.from_username(loader.context, username)
+        followers = profile.get_followers()
+        return {
+            'status': 'OK', 
+            'followers': [{
+                'username': str(follower.username),
+                'name': str(follower.full_name),
+                'profile_pic': str(follower.profile_pic_url),
+            } for follower in followers]
+        }, 200
+    except Exception as e:
+        print(e)
+        return {'status': 'Invalid request ' + str(e)}, 404
 
-    for post in post_iterator:
-        if counter <= max :
-            print("making a post serialisable" + str(counter))
-            post_serial = {
-                "key": post.shortcode,
-                "media" : post.url,
-                "likes" : post.likes,
-                "comments" : post.comments,
-                "caption" : post.caption,
-                "hashtags" : post.caption_hashtags,
-                "location": str(post.location),
-                "time": str(post.date.now()),
-                "media_count" : post.mediacount,
-                "mentions" : post.caption_mentions,
-            }
-            post_serialisable_list.append(post_serial)
-            counter = counter + 1
-        else :
-            break
+@app.route('/api/v1/profile/following/', methods=['GET'])
+def profile_following():
+    username = request.args['username']
+    cookie = json.loads(request.data)
+    filename = './sessions/' + username
+    saveSession(cookie, filename)
+    try:
+        loader.load_session_from_file(username=username, filename='./sessions/' + username)
+        profile = Profile.from_username(loader.context, username)
+        following = profile.get_followees()
+        return {
+            'status': 'OK', 
+            'following': [{
+                'username': str(following.username),
+                'name': str(following.full_name),
+                'profile_pic': str(following.profile_pic_url),
+            } for following in following]
+        }, 200
+    except Exception as e:
+        print(e)
+        return {'status': 'Invalid request'}, 404
 
-    respon = jsonify(post_serialisable_list)
-    return respon
+@app.route('/api/v1/profile/media/', methods=['GET'])
+def profile_media():
+    username = request.args['username']
+    cookie = json.loads(request.data)
+    filename = './sessions/' + username
+    saveSession(cookie, filename)
+    try:
+        loader.load_session_from_file(username=username, filename='./sessions/' + username)
+        profile = Profile.from_username(loader.context, username)
+        media = profile.get_posts()
+        return {
+            'status': 'OK', 
+            'media': [{
+                'id': str(media.shortcode),
+                'likes': str(media.likes),
+                'comments': str(media.comments),
+                'date': str(media.date),
+                'caption': str(media.caption),
+                'url': str(media.url),
+            } for media in media]
+        }, 200
+    except Exception as e:
+        print(e)
+        return {'status': 'Invalid request'}, 404
+
+@app.route('/api/v1/profile/liked/', methods=['GET'])
+def profile_liked():
+    username = request.args['username']
+    cookie = json.loads(request.data)
+    filename = './sessions/' + username
+    saveSession(cookie, filename)
+    try:
+        loader.load_session_from_file(username=username, filename='./sessions/' + username)
+        profile = Profile.from_username(loader.context, username)
+        liked = profile.get_liked_medias()
+        return {
+            'status': 'OK', 
+            'liked': [{
+                'id': str(media.shortcode),
+                'likes': str(media.likes),
+                'comments': str(media.comments),
+                'date': str(media.date),
+                'caption': str(media.caption),
+                'url': str(media.url),
+            } for media in liked]
+        }, 200
+    except Exception as e:
+        print(e)
+        return {'status': 'Invalid request'}, 404
+
+@app.route('/api/v1/profile/comment/', methods=['GET'])
+def profile_comment():
+    username = request.args['username']
+    cookie = json.loads(request.data)
+    filename = './sessions/' + username
+    saveSession(cookie, filename)
+    try:
+        loader.load_session_from_file(username=username, filename='./sessions/' + username)
+        profile = Profile.from_username(loader.context, username)
+        comments = profile.get_comments()
+        return {
+            'status': 'OK', 
+            'comments': [{
+                'id': str(comment.shortcode),
+                'likes': str(comment.likes),
+                'comments': str(comment.comments),
+                'date': str(comment.date),
+                'caption': str(comment.caption),
+                'url': str(comment.url),
+            } for comment in comments]
+        }, 200
+    except Exception as e:
+        print(e)
+        return {'status': 'Invalid request'}, 404
 
 
+@app.route('/api/v1/analytics/ghost/', methods=['GET'])
+def analytics_ghost():
+    username = request.args['username']
+    cookie = json.loads(request.data)
+    filename = './sessions/' + username
+    saveSession(cookie, filename)
+    try:
+        loader.load_session_from_file(username=username, filename='./sessions/' + username)
+        profile = Profile.from_username(loader.context, username)
 
+        likes = set()
+        print("Fetching likes of all posts of profile {}.".format(profile.username))
+        for post in profile.get_posts()[:4]:
+            print(post)
+            likes = likes | set(post.get_likes())
+        
+        print("Fetching followers of profile {}.".format(profile.username))
+        followers = set(profile.get_followers())
+        ghosts = followers - likes
 
-
-
-# DEPRERICATED Routes ----------------------------------------------------------------------------------
-
-# @app.route('/session/', methods=['POST'])
-# def setSession():
-#     username = request.args['q']
-#     crftoken = request.args['token']
-#     ds_user_id = request.args['id']
-#     ig_cb = request.args['ig']
-#     mid = request.args['mid']
-#     sessionid = request.args['session']
-#     ig_did = request.args['did']
-#     shbid = request.args['bid']
-#     shbts = request.args['bts']
-#     rur = request.args['rur']
-
-#     # Session object
-#     session = {
-#         'sessionid': sessionid, 
-#         'mid': mid, 
-#         'ig_pr': '1',
-#         'ig_nrcb' : ig_cb,
-#         'ig_vw': '1920', 
-#         'csrftoken': crftoken,
-#         's_network': '', 
-#         'ds_user_id': ds_user_id,
-#         'ig_did' : ig_did,
-#         'shbid' : shbid,
-#         'rur' : rur,
-#         'shbts' : shbts
-#     }
-
-#     filename = './sessions/' + username
-#     dirname = os.path.dirname(filename)
-
-#     if dirname != '' and not os.path.exists(dirname):
-#             os.makedirs(dirname)
-            
-#     with open(filename, 'wb') as sessionfile:
-#         pickle.dump(session, sessionfile)
-#         print("saved session file")
-
-#     loader.load_session_from_file(username=username, filename='./sessions/' + username)
-#     s_username = loader.test_login()
-#     if s_username == username:
-#         return 200
-#     else:
-#         return 104
-
-# @app.route('/session/check/', methods=['GET'])
-# def checkSession():
-#     username = request.args['q']
-#     loader.load_session_from_file(username=username, filename='./sessions/' + username)
-#     s_username = str(loader.test_login())
-#     if s_username == username:
-#         return str(200)
-#     else:
-#         return str(104)
-
-
-    
+        return {
+            'status': 'OK', 
+            'ghost': [{
+                'id': str(ghost.shortcode),
+                'likes': str(ghost.likes),
+                'comments': str(ghost.comments),
+                'date': str(ghost.date),
+                'caption': str(ghost.caption),
+                'url': str(ghost.url),
+            } for ghost in ghosts]
+        }, 200
+    except Exception as e:
+        print(e)
+        return {'status': 'Invalid request'}, 404
 
